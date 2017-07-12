46de25900c68d628f38745d1203f7dc818151456
dyy <771750834@qq.com>
2017-07-12 11:03:05 +0800

    init

---
+++ DYYDownLoader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYYDownLoader
+//
+//  Created by yang on 2017/7/12.
+//  Copyright © 2017年 dingyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYYDownLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYYDownLoader
+//
+//  Created by yang on 2017/7/12.
+//  Copyright © 2017年 dingyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
